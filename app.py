84d6cdcee3a0a4d69a2cf71aa8a3551141d542f8
leelakrishna-cloud <leelakrishnan.t@gmail.com>
2026-04-10 17:55:29 +0800

    Add warning filter to suppress warnings
    Suppress warnings by filtering them out.

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import warnings
+warnings.filterwarnings("ignore")
+
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import SentenceTransformerEmbeddings
